66e20c871062e4bb41d40bd8c99b8cf72f9beb39
unknown <debymanalu2607@gmail.com>
2023-11-24 09:54:14 +0700

    file tambahan

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// Contoh script JavaScript
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Halaman selesai dimuat.');
+    // Tambahkan logika JavaScript lainnya sesuai kebutuhan.
+});
